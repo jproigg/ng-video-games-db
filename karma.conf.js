4f46e41a3163dbae56a388bed5645d5893a48fc3
jproigg <jproig2711@gmal.com>
2022-03-05 00:36:26 -0600

    commit

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,44 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-junit-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
- reporters: ['junit'],    
-    junitReporter: { 
- outputDir: 'karma-results',    
- outputFile: 'karma-results.xml'    
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    angularCli: {
-      environment: 'dev',
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/ng-video-game-db'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
+    colors: true,
     logLevel: config.LOG_INFO,
- browsers: ['ChromeHeadless'],    
- autoWatch: false,    
- singleRun: true    
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
